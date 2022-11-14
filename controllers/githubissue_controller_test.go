124bc73280b25008f5bb174cee1366a680938b92
Carlo Lobrano <c.lobrano@gmail.com>
2022-11-14 13:40:46 +0100

    Initial Reconcile logic with mock Github access
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- controllers/githubissue_controller_test.go
+++ controllers/githubissue_controller_test.go
@@ -7,6 +7,13 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 var _ = Describe("GithubissueController", func() {
@@ -14,23 +21,12 @@ var _ = Describe("GithubissueController", func() {
 	Context("default", func() {
 		var underTest *v1alpha1.GithubIssue
 		var (
-			expected_url         = "clearly invalid repo URL"
+			expected_url         = "https://github.com/clobrano/githubissues-operator"
 			expected_title       = "Op issue title"
 			expected_description = "Op issue title"
 		)
 		BeforeEach(func() {
-			underTest = &v1alpha1.GithubIssue{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test",
-					Namespace: "default",
-				},
-				Spec: v1alpha1.GithubIssueSpec{
-					Repo:        expected_url,
-					Title:       expected_title,
-					Description: expected_description,
-				},
-			}
-
+			underTest = newGithubIssue(expected_title, expected_description)
 			err := k8sClient.Create(context.Background(), underTest)
 			Expect(err).NotTo(HaveOccurred())
 		})
@@ -48,4 +44,107 @@ var _ = Describe("GithubissueController", func() {
 			})
 		})
 	})
+
+	Context("Reconciliation", func() {
+		var (
+			underTest *v1alpha1.GithubIssue
+			client    client.WithWatch
+			sch       *runtime.Scheme
+		)
+		BeforeEach(func() {
+			underTest = newGithubIssue("first issue", "issue has been assigned")
+			objs := []runtime.Object{underTest}
+			sch = scheme.Scheme
+			sch.AddKnownTypes(v1alpha1.SchemeBuilder.GroupVersion, underTest)
+			client = fake.NewFakeClient(objs...)
+		})
+
+		When("the issue does not exist", func() {
+			gclient := newGithubFakeClient([]GithubTicket{})
+			Expect(gclient.SpyTicket).To(BeNil())
+
+			It("it should create it", func() {
+				r := &GithubIssueReconciler{client, sch, &gclient}
+				req := reconcile.Request{
+					NamespacedName: types.NamespacedName{
+						Name:      "test",
+						Namespace: "default",
+					},
+				}
+				_, err := r.Reconcile(context.TODO(), req)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(gclient.SpyTicket).ToNot(BeNil())
+				Expect(gclient.SpyTicket.Title).To(Equal("first issue"))
+				Expect(gclient.SpyTicket.Description).To(Equal("issue has been assigned"))
+			})
+		})
+
+		When("the issue exists without the expected description", func() {
+			gclient := newGithubFakeClient([]GithubTicket{
+				{"first issue", "first big issue"},
+				{"second issue", "second issue description"},
+			})
+			// The first issue in the mock-ed repository is expected to be modified
+			gclient.SpyTicket = &gclient.Tickets[0]
+
+			It("it should update the ticket description", func() {
+				r := &GithubIssueReconciler{client, sch, &gclient}
+				req := reconcile.Request{
+					NamespacedName: types.NamespacedName{
+						Name:      "test",
+						Namespace: "default",
+					},
+				}
+				_, err := r.Reconcile(context.TODO(), req)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(gclient.SpyTicket.Title).To(Equal("first issue"))
+				Expect(gclient.SpyTicket.Description).To(Equal("issue has been assigned"))
+			})
+		})
+
+		When("the issue has a PR", func() {
+			It("status should be updated", func() {
+
+			})
+		})
+	})
 })
+
+func newGithubIssue(title, description string) *v1alpha1.GithubIssue {
+	return &v1alpha1.GithubIssue{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.GithubIssueSpec{
+			Repo:        "https://github.com/clobrano/githubissues-operator",
+			Title:       title,
+			Description: description,
+		},
+	}
+}
+
+type GithubFakeClient struct {
+	// Tickets mocks a Github repository issue list
+	Tickets   []GithubTicket
+	SpyTicket *GithubTicket
+}
+
+func newGithubFakeClient(tickets []GithubTicket) GithubFakeClient {
+	return GithubFakeClient{tickets, nil}
+}
+
+func (g *GithubFakeClient) GetTickets() ([]GithubTicket, error) {
+	return g.Tickets, nil
+}
+
+func (g *GithubFakeClient) CreateTicket(t GithubTicket) error {
+	g.SpyTicket = &t
+	return nil
+}
+
+func (g *GithubFakeClient) UpdateTicket(t GithubTicket) error {
+	g.SpyTicket.Title = t.Title
+	g.SpyTicket.Description = t.Description
+	return nil
+}
